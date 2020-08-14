555a3bf542559b134c1662eda44476916f294420
Leon <leonalispringer@gmail.com>
2020-08-14 05:35:26 +0300

    This is the solution for Hackerrank_Maximize_It challenge

---
+++ Maximize_It.py
@@ -0,0 +1,19 @@
+'''
+this script is solution for Hackerrank challenge
+'''
+
+credentials = input('Enter K and M values: ').split()
+lists  = []
+crd = list(map(int,credentials))
+for i in range(crd[0]):
+    lst = input('input list values: ').split()
+    lst_i = list(map(int,lst))
+    lists.append(lst_i[1:])
+    
+from itertools import product
+kartezyen = list(product(*lists))    
+
+def func(nums):
+    return sum(x*x for x in nums) % crd[1]
+
+print(max(list(map(func, kartezyen))))
